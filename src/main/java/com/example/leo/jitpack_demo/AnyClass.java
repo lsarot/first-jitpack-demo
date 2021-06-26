60bfcfa63b90631324c7548d5a36f7f4ac196ca9
leosar <ldosto>
2021-06-26 15:07:20 +0200

    refactor: latest changes without importance

--- src/main/java/com/example/leo/jitpack_demo/AnyClass.java
+++ src/main/java/com/example/leo/jitpack_demo/AnyClass.java
@@ -1,5 +1,9 @@
 package com.example.leo.jitpack_demo;
 
+/**
+ * ESTE PROYECTO LO VAMOS A IMPORTAR COMO DEPENDENCIA USANDO MAVEN
+ * Jitpack nos facilita la tarea dándole acceso al repositorio Git simplemente.
+ * */
 public class AnyClass {
 
 	public void anyMethod() {
